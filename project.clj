d214bfff2ca84fbafa58ac901f00ab7b709322c1
Adam Harper <github@adam-harper.com>
2015-05-18 11:01:43 +0100

    Version 1.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ifs-part-search "1.0.6"
+(defproject ifs-part-search "1.0.7-SNAPSHOT"
   :description "A web API for searching for parts within IFS."
   :url "https://github.com/lymingtonprecision/ifs-part-search"
   :license {:name "Eclipse Public License"
